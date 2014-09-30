50d98777e5d548d1d263b0d96778e92e27baefd2
Roland Vachter <roland.vachter@ft.com>
2014-09-30 15:15:13 +0300

    messageQueue is made collectionId aware by instances. Documentation updated.

--- src/javascripts/config.js
+++ src/javascripts/config.js
@@ -1,3 +1,7 @@
 var EnvConfig = require('comment-utilities').EnvConfig;
 
+/**
+ * Export a single instance of the config container.
+ * @type {EnvConfig}
+ */
 module.exports = new EnvConfig();
--- src/javascripts/i18n.js
+++ src/javascripts/i18n.js
@@ -1,3 +1,7 @@
+/**
+ * Error message override.
+ * @type {Object}
+ */
 exports.errors = {
     emptyComment: "It seems you're attempting to post an empty comment."
 };
