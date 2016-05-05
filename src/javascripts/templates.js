6c31a8155ad7b9197896e7aee3b4ed236ef9fe8b
Roland Vachter <roland.vachter@iquestgroup.com>
2016-05-05 10:10:10 +0300

    Use templating engine exposed by o-comment-ui

--- src/javascripts/templates.js
+++ src/javascripts/templates.js
@@ -1,14 +1,14 @@
-const hogan = require('hogan');
+const oCommentUi = require('o-comment-ui');
 
 /**
  * List of the templates available.
  * @type {Object}
  */
 module.exports = {
-	editor: hogan.compile(require('../templates/editor.html')),
-	comments: hogan.compile(require('../templates/comments.html')),
-	comment: hogan.compile(require('../templates/comment.html')),
-	signIn: hogan.compile(require('../templates/signIn.html')),
-	loggedIn: hogan.compile(require('../templates/loggedIn.html')),
-	notification: hogan.compile(require('../templates/notification.html'))
+	editor: oCommentUi.templatingEngine.compile(require('../templates/editor.html')),
+	comments: oCommentUi.templatingEngine.compile(require('../templates/comments.html')),
+	comment: oCommentUi.templatingEngine.compile(require('../templates/comment.html')),
+	signIn: oCommentUi.templatingEngine.compile(require('../templates/signIn.html')),
+	loggedIn: oCommentUi.templatingEngine.compile(require('../templates/loggedIn.html')),
+	notification: oCommentUi.templatingEngine.compile(require('../templates/notification.html'))
 };
